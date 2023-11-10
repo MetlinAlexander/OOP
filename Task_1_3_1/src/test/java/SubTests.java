eecebe14db90ff475a2cb742e0ee529468dcf893
MetlinAlexander <alexmet04@gmail.com>
2023-11-10 09:44:19 +0700

    move txt files to resources

--- Task_1_3_1/src/test/java/SubTests.java
+++ Task_1_3_1/src/test/java/SubTests.java
@@ -10,7 +10,7 @@ public class SubTests {
     void sampleTest() {
         ArrayList<Integer> realAnswer = new ArrayList<>(Arrays.asList(1, 8));
         try {
-            ArrayList<Integer> answer = SubFinder.find("src/test/java/textfile.txt", "bra");
+            ArrayList<Integer> answer = SubFinder.find("src/test/resources/textfile.txt", "bra");
             Assertions.assertEquals(realAnswer, answer);
         } catch (Exception ex) {
             fail();
@@ -21,7 +21,7 @@ public class SubTests {
     void bigTest() {
         ArrayList<Integer> realAnswer = new ArrayList<>(Arrays.asList(1381, 24270, 32449, 33205, 34820, 38215, 38907, 43183, 51187, 53293, 60978, 75115, 98199, 108590, 110625, 117638));
         try {
-            ArrayList<Integer> answer = SubFinder.find("src/test/java/24.txt", "ftw");
+            ArrayList<Integer> answer = SubFinder.find("src/test/resources/24.txt", "ftw");
             Assertions.assertEquals(realAnswer, answer);
         } catch (Exception ex) {
             fail();
