49c1bd232ed754f067aa7a9e75d8401291671ffc
MetlinAlexander <MetlinAlexander@users.noreply.github.com>
2023-11-19 12:31:41 +0000

    Deploying to gh-pages from @ MetlinAlexander/OOP@4d7448ae77455f84bd592c9175b8bf9648ba93f6 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Grade"},{"p":"<Unnamed>","l":"GradeBook"},{"p":"<Unnamed>","l":"Semester"},{"p":"<Unnamed>","l":"Subject"}]
