8c079d8a5d5abf55975be09ed1dab3e6798d299a
MetlinAlexander <MetlinAlexander@users.noreply.github.com>
2024-02-21 15:39:28 +0000

    Deploying to gh-pages from @ MetlinAlexander/OOP@f9b15ef92d2ecda27f16d895bd2ec1c00f62a470 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"MultiThreadCalc"},{"p":"<Unnamed>","l":"ParallelStreamCalc"},{"p":"<Unnamed>","l":"Primes"},{"p":"<Unnamed>","l":"SequentialCalc"}]
